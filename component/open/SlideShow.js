f652c0ee55cad75ce19038ab3c216b481f7afdde
nguyenvantu1911 <tunvph24387@fpt.edu.vn>
2023-06-27 23:57:14 +0700

    tu

--- component/open/SlideShow.js
+++ component/open/SlideShow.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { View, Image } from 'react-native';
 import Swiper from 'react-native-swiper';
 
@@ -9,15 +9,29 @@ const images = [
 ];
 
 const SlideShow = () => {
+  
+    
+    const [data, setData] = useState([]);
+
+  useEffect(() => {
+    let api_url ="https://xuantuan06.000webhostapp.com/php/GetTourFromApi.php"
+    fetch(api_url).then(response => response.json())
+    .then(data => setData(data))
+    .catch(error => {
+      console.error(error);
+    })}, []);
+    console.log(data);
+   
+
   return (
     <Swiper loop={true} autoplay={true}>
-      {images.map((image, index) => (
+      {data.map((item, index) => (
         <View key={index}>
-          <Image source={image} style={{ width: '100%', height: 200, borderRadius:10 }} />
+          <Image source={{uri: item.image}} style={{ width: '100%', height: 200, borderRadius:10 }} />
         </View>
       ))}
     </Swiper>
   );
-};
+      };
 
 export default SlideShow;
